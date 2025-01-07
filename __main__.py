cb225872c0e9fb6518edd814c54e272391a312b3
OpenJ92 <jacob.vartuli.92@gmail.com>
2025-01-06 20:26:53 -0500

    resolve upstream changes

--- __main__.py
+++ __main__.py
@@ -4,21 +4,6 @@ from starcraft_data_orm.warehouse import initialize_warehouse
 from sqlalchemy.sql import text
 
 import asyncio
-<<<<<<< Updated upstream
-import sc2reader
-from sc2reader.engine.plugins import (
-    SelectionTracker,
-    APMTracker,
-    ContextLoader,
-    GameHeartNormalizer,
-)
-
-sc2reader.engine.register_plugin(SelectionTracker())
-sc2reader.engine.register_plugin(APMTracker())
-sc2reader.engine.register_plugin(ContextLoader())
-sc2reader.engine.register_plugin(GameHeartNormalizer())
-=======
->>>>>>> Stashed changes
 
 async def main():
     initialize_warehouse()
